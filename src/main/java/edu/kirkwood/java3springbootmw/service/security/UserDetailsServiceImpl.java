63abf95fe1901c855b01f05877dd2f10b0c8b956
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-12-03 12:36:58 -0600

    Dec 3 - Registration and Login API works

--- src/main/java/edu/kirkwood/java3springbootmw/service/security/UserDetailsServiceImpl.java
+++ src/main/java/edu/kirkwood/java3springbootmw/service/security/UserDetailsServiceImpl.java
@@ -5,7 +5,9 @@ import edu.kirkwood.java3springbootmw.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
